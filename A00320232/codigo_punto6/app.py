9a28500512d6defc48e5fadaea54b9252a418a38
operativos <jeffrykr95@gmail.com>
2018-05-04 13:29:42 -0500

    Update app.py

--- A00320232/codigo_punto6/app.py
+++ A00320232/codigo_punto6/app.py
@@ -6,9 +6,9 @@ from flask import Flask, request
 
 @cli.app.CommandLineApp
 def app(app):
-    slack_token = 'xoxp-356698734240-356698734544-357190918564-49c3e5624e9da4dda1d7f74b8b7285bb' 
+    slack_token = 'xoxp-357964064544-358115947873-359555646406-da306896a6f1df139e6f97ecd35134e7' 
     sc = SlackClient(slack_token)
-    sc.api_call( "chat.postMessage", channel="so-exam2",text =str(psutil.cpu_times())+"\n\n"+str(psutil.cpu_percent(interval=1, percpu=True))+"\n\n"+str(psutil.cpu_stats())+"\n\n"+str(psutil.virtual_memory())+"\n\n"+str(psutil.swap_memory())+"\n\n"+str(psutil.diskpartitions())+str(psutil.disk_usage('/'))))   
+    sc.api_call( "chat.postMessage", channel="test-exam",text =str(psutil.cpu_times())+"\n\n"+str(psutil.cpu_percent(interval=1, percpu=True))+"\n\n"+str(psutil.cpu_stats())+"\n\n"+str(psutil.virtual_memory())+"\n\n"+str(psutil.swap_memory())+"\n\n"+str(psutil.disk_partitions())+str(psutil.disk_usage('/')))   
 
 if __name__ == "__main__":
-app.run()
+    app.run()
